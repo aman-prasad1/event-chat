0730c27f899b0ce022550b187023809e815826f4
Aman Prasad <amanprasad048@gmail.com>
2026-04-29 21:10:32 +0530

    feat: add login api

--- backend/src/models/auth/controller.js
+++ backend/src/models/auth/controller.js
@@ -1,12 +1,30 @@
 import { prisma } from '../../db/index.js';
 import bcrypt from 'bcrypt';
 import fs from 'fs';
+import jwt from 'jsonwebtoken';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import { ApiError } from '../../utils/ApiError.js';
 import { ApiResponse } from '../../utils/ApiResponse.js';
 import { uploadOnCloudinary } from '../../utils/cloudinary.js';
 import { UserType } from './types.js';
 
+
+const generateAccessToken = (user) => {
+    const payload = {
+        id: user.id,
+        username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name
+    };
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION });
+};
+const generateRefreshToken = (user) => {
+    const payload = {
+        id: user.id,
+        username: user.username
+    };
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION });
+};
 
 const register = asyncHandler(async (req, res) => {
     try {
@@ -67,7 +85,7 @@ const register = asyncHandler(async (req, res) => {
         res
             .status(201)
             .json(
-                new ApiResponse(201, createdUser, 'User registered successfully')
+                new ApiResponse(201, 'User registered successfully', createdUser)
             );
 
     } catch (error) {
@@ -82,4 +100,72 @@ const register = asyncHandler(async (req, res) => {
 });
 
 
-export { register,}
+const login = asyncHandler(async (req, res) => {
+    
+    try {
+        const { username, password } = req.body;
+    
+        // Validate that all fields are provided and not empty
+        if ([username, password].some(field => !field || field.trim() === '')) {
+            throw new ApiError(400, 'All fields are required');
+        }
+    
+        // Check if the user exists
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (!user) {
+            throw new ApiError(400, 'Invalid username or password');
+        }
+    
+        // Compare the provided password with the hashed password in the database
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new ApiError(400, 'Invalid username or password');
+        }
+    
+        // Generate access and refresh tokens
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+    
+        // Store the refresh token in the database
+        await prisma.user.update({
+            where: { id: user.id },
+            data: { refresh_Token: refreshToken }
+        });
+        
+        
+        // Prepare the response data without sensitive information
+        const responseData = {
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            avatar_url: user.avatar_url,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        };
+    
+        const options = {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000 * 7,
+            path: '/'
+        }
+    
+        res
+            .status(200)
+            .cookie('refreshtoken', refreshToken, options)
+            .cookie('accesstoken', accessToken, options)
+            .json(
+                new ApiResponse(200, 'Login successful', responseData)
+            );
+    } catch (error) {
+        throw new ApiError(error.statusCode || 500, error.message || 'Internal Server Error');
+    }
+});
+
+
+
+export { 
+    register,
+    login,
+}
--- backend/src/models/auth/router.js
+++ backend/src/models/auth/router.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { upload } from '../../middlewares/multer.middleware.js';
-import { register } from './controller.js';
+import { 
+    register,
+    login
+} from './controller.js';
 
 const router = Router();
 
 router.post('/register', upload.single('avatar'), register);
+router.post('/login', login);
+
 
 export default router;
