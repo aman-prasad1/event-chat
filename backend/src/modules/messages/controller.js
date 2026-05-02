4b950a20d09d95eb81e56c112a58810884c51575
Aman Prasad <amanprasad048@gmail.com>
2026-05-02 18:19:45 +0530

    feat(messages): add POST /messages endpoint

--- backend/src/modules/messages/controller.js
+++ backend/src/modules/messages/controller.js
@@ -2,6 +2,8 @@ import { prisma } from '../../db/index.js';
 import { ApiError } from '../../utils/ApiError.js';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import { ApiResponse } from '../../utils/ApiResponse.js';
+import { uploadOnCloudinary } from '../../utils/cloudinary.js';
+import fs from 'fs';
 
 const createDirectConversation = asyncHandler(async (req, res) => {
     try {
@@ -96,7 +98,98 @@ const getConverstionMessages = asyncHandler(async (req, res) => {
 
 });
 
+const createConversationMessage = asyncHandler(async (req, res) => {
+    try {
+        const { conversationId, content, type } = req.body;
+
+        // Membership validation
+        const member = await prisma.conversationMember.findFirst({
+            where: {
+                conversationId,
+                userId: req.user.id
+            }
+        });
+        if (!member) {
+            throw new ApiError(403, 'You are not a member of this conversation');
+        }
+
+        let parsedContent;
+
+        // Handle message types
+        if (type === 'text') {
+            if (req.file) {
+                throw new ApiError(400, 'File should not be included for text messages');
+            }
+
+            try {
+                parsedContent = typeof content === 'string' ? JSON.parse(content) : content;
+            } catch (error) {
+                throw new ApiError(400, 'Invalid content format for text message');
+            }
+
+            if (
+                typeof parsedContent !== 'object' ||
+                typeof parsedContent.text !== 'string' ||
+                parsedContent.text.trim() === ''
+            ) {
+                throw new ApiError(400, 'Text content must be a non-empty string');
+            }
+        } else if (type === 'file') {
+            if (!req.file) {
+                throw new ApiError(400, 'File is required for file messages');
+            }
+
+            // upload file to cloudinary
+            const uploadResult = await uploadOnCloudinary(req.file.path, "chatFiles");
+            if (!uploadResult || !uploadResult.url || !uploadResult.public_id) {
+                throw new ApiError(500, 'Failed to upload file');
+            }
+
+            parsedContent = {
+                filename: req.file.originalname,
+                file_url: uploadResult.url,
+                file_publicId: uploadResult.public_id,
+                mimetype: req.file.mimetype,
+                size: req.file.size
+            };
+        } else {
+            throw new ApiError(400, 'Unsupported message type');
+        }
+
+        const message = await prisma.message.create({
+            data: {
+                conversationId,
+                senderId: req.user.id,
+                content: parsedContent,
+                type
+            }
+        });
+
+        fs.unlink(req.file.path, (err) => {
+            console.error('Error deleting local file:', err);
+        });
+
+        res
+            .status(201)
+            .json(new ApiResponse(201, 'Message sent successfully', { "messageId": message.id }));
+    } catch (error) {
+        if (req.file) {
+            // Ensure that the local file is deleted after processing
+            fs.unlink(req.file.path, (err) => {
+                console.error('Error deleting local file:', err);
+            });
+        }
+
+        if (error.code === "P2003") {
+            throw new ApiError(404, 'Conversation not found');
+        }
+
+        throw new ApiError(error.statusCode || 500, error.message || 'Failed to send message');
+    }
+});
+
 export {
     createDirectConversation,
-    getConverstionMessages
+    getConverstionMessages,
+    createConversationMessage
 }
--- backend/src/modules/messages/router.js
+++ backend/src/modules/messages/router.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { authenticateAccessToken } from '../../middlewares/auth.middleware.js';
+import { upload } from '../../middlewares/multer.middleware.js';
 import { 
     createDirectConversation,
-    getConverstionMessages
+    getConverstionMessages,
+    createConversationMessage
  } from './controller.js';
 
 const router = Router();
 
 router.post('/conversations/direct', authenticateAccessToken, createDirectConversation);
+router.post('/', authenticateAccessToken, upload.single('file'), createConversationMessage);
 router.get('/:conversationId', authenticateAccessToken, getConverstionMessages);
 
 export default router;
