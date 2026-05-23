be48b1817c962ac3e4c35457e8ff8b208f193983
Aman Prasad <amanprasad048@gmail.com>
2026-05-23 15:20:21 +0530

    feat(auth): add logout API

--- backend/src/modules/auth/router.js
+++ backend/src/modules/auth/router.js
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import { 
     register,
     login,
-    refreshTokens
+    refreshTokens,
+    logout
 } from './controller.js';
 import { authLimiter, registerLimiter } from '../../middlewares/rateLimiter.middleware.js';
+import { authenticateAccessToken } from '../../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.post('/register', registerLimiter, register);
 router.post('/login', authLimiter, login);
 router.post('/refresh-tokens', refreshTokens);
+router.post('/logout', authenticateAccessToken, logout);
 
 export default router;
