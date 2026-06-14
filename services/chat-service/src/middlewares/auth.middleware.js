2777f60520b722c8c03e7f7878e61cba8abb0f3b
Aman Prasad <amanprasad048@gmail.com>
2026-06-14 23:51:47 +0530

    feat: implement token blacklisting using Redis in auth middleware

--- services/chat-service/src/middlewares/auth.middleware.js
+++ services/chat-service/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { prisma } from "../db/index.js";
+import { redisClient } from "../redis/index.js";
 import jwt from "jsonwebtoken";
 
 export const authenticateAccessToken = async (req, res, next) => {
@@ -9,15 +10,20 @@ export const authenticateAccessToken = async (req, res, next) => {
         return next(new ApiError(401, "Unauthorized: No token provided"));
     }
 
+    // check if token is blacklisted
+    const isBlacklisted = await redisClient.get(`blacklisted:${token}`);
+    if(isBlacklisted) {
+        return next(new ApiError(401, "Unauthorized: Token is blacklisted"));
+    }
+
     try {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const user = await prisma.user.findUnique({ where: { id: decodedToken.id } });
 
-        if (!user) {
+        if (!decodedToken?.id) {
             return next(new ApiError(401, "Unauthorized: User not found"));
         }
 
-        req.user = user;
+        req.user = decodedToken;
         next();
     } catch (err) {
         return next(new ApiError(401, "Unauthorized: Invalid token"));
